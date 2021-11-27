9c83ffa87d6c6f34c9849a7c30668dbb79d986a1
Andrei Todor <atodor@pentalog.com>
2021-11-27 15:34:02 +0200

    redirect frontend route to /

--- src/main/java/com/example/DungeonsDragons/configuration/WebConfiguration.java
+++ src/main/java/com/example/DungeonsDragons/configuration/WebConfiguration.java
@@ -2,10 +2,16 @@ package com.example.DungeonsDragons.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/create-character")
+                .setViewName("forward:/");
+    }
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("*");
     }
